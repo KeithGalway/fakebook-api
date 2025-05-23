343c0487e6bb17a5fcc22395df6cd515bb8171ef
KeithGalway <keith.galway@outlook.com>
2025-05-24 03:31:23 +0800

    Update posts.js
    fixed getFeedPosts function to get all posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,8 +29,7 @@ export const createPost = async (req, res) => {
 // READ
 export const getFeedPosts = async (req, res) => {
     try {
-        const { userId } = req.params
-        const post = await Post.find({ userId })
+        const post = await Post.find()
         res.status(200).json(post)
     } catch (err) {
         res.status(404).json({ message: err.message })
